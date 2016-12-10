6364813c6da1428d258a18127d074bf8f84d0764
Melissa Garcia <melissag13@gmail.com>
2016-12-10 18:11:36 -0500

    First commit

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+	<title>Clueless</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+	<script src="https://code.jquery.com/jquery-3.1.1.min.js" integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=" crossorigin="anonymous"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+	<!-- Latest compiled and minified JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+</head>
+<body>
+	<div class="container">
+
+		<div class="jumbotron" style="background-color:transparent">
+			
+			<center><img src="./assets/images/mannequin1.jpg")">
+			<h1 style="color:hotpink;">Clueless?</h1>
+			</center>
+		</div>
+
+	<footer class="footer" style="text-align:center">
+	    <div class="container">
+		        <p>Melissa Garcia Copyright &copy; 
+		    <script language="javascript" type="text/javascript">
+					var today = new Date()
+					var year = today.getFullYear()
+					document.write(year)
+			</script></p>
+	    </div>
+	</footer>
+
+
+	</div>
+
+</body>
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        <script src="https://code.jquery.com/jquery-3.1.1.min.js" integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=" crossorigin="anonymous"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+        <!-- Latest compiled and minified JavaScript -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+        
+
+    </head>
+    <body>
+        <div class="jumbotron" style="background-color:transparent">
+            
+            <center><img src="{{ URL::asset('images/mannequin1.jpg') }}">
+            <h1 style="color:hotpink;">Clueless?</h1>
+            <h3>Your answer to the daunting question:  "What am I going to wear?"</h3>
+            <br>
+            <br>
+            <br>
+            <br>
+            
+            
+
+            <div class="">
+                <a href="./survey.html"><button type="button" class="btn btn-default" ><h4>Log In</h4></button></a>
+            
+                <a href="./survey.html"><button type="button" class="btn btn-default" ><h4>Sign Up</h4></button></a>
+            </div>
+        </center>
+        </div>
+
+        <footer class="footer" style="text-align:center">
+            <div class="container">
+                <p>Melissa Garcia Copyright &copy; 
+                <script language="javascript" type="text/javascript">
+                    var today = new Date()
+                    var year = today.getFullYear()
+                    document.write(year)
+                </script></p>
+          </div>
+        </footer>
+
+        </div>
+    </body>
+</html>
